29885f7df537dd48ede6eeeecdcf34f1cde5f028
paklong <paklong2556@gmail.com>
2025-03-10 19:09:25 -0700

    style(card): adjust card margin
    - Increase Card component margin from m-4 to m-6 for better spacing

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ export default function Card({ id, src, onClick, disable }) {
   return (
     <input
       type="image"
-      className="m-4 rounded-lg max-w-36 border-8"
+      className="m-6 rounded-lg max-w-36 border-8"
       src={src}
       onClick={() => onClick(id)}
       disabled={disable}
